ccf420d7f2cb55390d445c90564655b648498b93
Sarah Ledford <Sarah_Ledford@outlook.com>
2021-03-05 14:33:43 -0600

    added a contact form

--- StoreFrontLab.UI.MVC/Controllers/HomeController.cs
+++ StoreFrontLab.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using StoreFrontLab.UI.MVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace StoreFrontLab.UI.MVC.Controllers
 {
@@ -19,12 +22,50 @@ namespace StoreFrontLab.UI.MVC.Controllers
             return View();
         }
 
-        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
-    }
-}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"Message from {cvm.Name} at {cvm.EmailAddress}<br />" +
+                $"Subject: {cvm.Subject}<br />" +
+                $"Message:<br />{cvm.Message}";
+
+            MailMessage mm = new MailMessage("admin@sarahledford.com", "Sarah_Ledford@outlook.com", cvm.Subject, message);
+
+            mm.IsBodyHtml = true;
+
+            mm.Priority = MailPriority.High;
+
+            mm.ReplyToList.Add(cvm.EmailAddress);
+
+            SmtpClient client = new SmtpClient("mail.sarahledford.com");
+
+            client.Credentials = new NetworkCredential("admin@sarahledford.com", "aKmS2FhiQp_");
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (System.Exception)
+            {
+                ViewBag.CustomerMessage = $"We're sorry. Your request could not be processed at this time.";
+
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+        }
+
+
+    }//end class
+}//end namespace
---
+++ StoreFrontLab.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoreFrontLab.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Email address is required")]
+        [EmailAddress(ErrorMessage = "* Please enter a valid email address")]
+        [Display(Name="Email")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "* Subject is required")]
+        public string Subject { get; set; }
+
+        [UIHint("MultilineText")]
+        [Required(ErrorMessage = "* Message is required")]
+        public string Message { get; set; }
+
+
+
+
+    }//end class
+}//namespace
