ccecdad5d7fc34ebf8911238ea3ee5f381718be6
Sarah Ledford <Sarah_Ledford@outlook.com>
2021-03-27 08:39:03 -0500

    Added full CRUD functionality to Studios!

--- StoreFrontLab.UI.MVC/Controllers/StudiosController.cs
+++ StoreFrontLab.UI.MVC/Controllers/StudiosController.cs
@@ -15,112 +15,169 @@ namespace StoreFrontLab.UI.MVC.Controllers
         private VintageMediaStoreEntities db = new VintageMediaStoreEntities();
 
         // GET: Studios
-        [Authorize(Roles ="Admin, Employee")]
+        [Authorize(Roles = "Admin, Employee")]
         public ActionResult Index()
         {
             return View(db.Studios.ToList());
         }
 
-        // GET: Studios/Details/5
-        [Authorize(Roles = "Admin, Employee")]
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Studio studio = db.Studios.Find(id);
-            if (studio == null)
-            {
-                return HttpNotFound();
-            }
-            return View(studio);
-        }
+        #region Original synchronous actions
+        //// GET: Studios/Details/5
+        //[Authorize(Roles = "Admin, Employee")]
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Studio studio = db.Studios.Find(id);
+        //    if (studio == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(studio);
+        //}
 
-        // GET: Studios/Create
-        [Authorize(Roles = "Admin, Employee")]
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Studios/Create
+        //[Authorize(Roles = "Admin, Employee")]
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: Studios/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //// POST: Studios/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin, Employee")]
+        //public ActionResult Create([Bind(Include = "StudioID,StudioName")] Studio studio)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Studios.Add(studio);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(studio);
+        //}
+
+        //// GET: Studios/Edit/5
+        //[Authorize(Roles = "Admin, Employee")]
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Studio studio = db.Studios.Find(id);
+        //    if (studio == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(studio);
+        //}
+
+        //// POST: Studios/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin, Employee")]
+        //public ActionResult Edit([Bind(Include = "StudioID,StudioName")] Studio studio)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(studio).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(studio);
+        //}
+
+        //// GET: Studios/Delete/5
+        //[Authorize(Roles = "Admin")]
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Studio studio = db.Studios.Find(id);
+        //    if (studio == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(studio);
+        //}
+
+        //// POST: Studios/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin")]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Studio studio = db.Studios.Find(id);
+        //    db.Studios.Remove(studio);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //} 
+        #endregion
+
+        //******************************************************AJAX*******************************************************************//
+
+        //*****************************************************DELETE******************************************************************//
+        //Delete a studio and return JSON data ONLY
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin, Employee")]
-        public ActionResult Create([Bind(Include = "StudioID,StudioName")] Studio studio)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Studios.Add(studio);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(studio);
-        }
-
-        // GET: Studios/Edit/5
-        [Authorize(Roles = "Admin, Employee")]
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Studio studio = db.Studios.Find(id);
-            if (studio == null)
-            {
-                return HttpNotFound();
-            }
-            return View(studio);
-        }
-
-        // POST: Studios/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin, Employee")]
-        public ActionResult Edit([Bind(Include = "StudioID,StudioName")] Studio studio)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(studio).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(studio);
-        }
-
-        // GET: Studios/Delete/5
-        [Authorize(Roles = "Admin")]
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Studio studio = db.Studios.Find(id);
-            if (studio == null)
-            {
-                return HttpNotFound();
-            }
-            return View(studio);
-        }
-
-        // POST: Studios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public ActionResult DeleteConfirmed(int id)
+        public JsonResult AjaxDelete(int id)
         {
             Studio studio = db.Studios.Find(id);
             db.Studios.Remove(studio);
             db.SaveChanges();
-            return RedirectToAction("Index");
+
+            string message = string.Format($"{studio.StudioName} has been deleted");
+            return Json(new { id = id, message = message });
+        }
+
+        //*****************************************************INFO********************************************************************//
+        //get a partial view for details using AJAX for display
+        //generate the parital with details scaffolding
+        [HttpGet]
+        public PartialViewResult StudioDetails(int id)
+        {
+            Studio studio = db.Studios.Find(id);
+            return PartialView(studio);
+        }
+
+        //*****************************************************CREATE********************************************************************//
+        //No need for a [get] b/c called partial view and passed empty studio object from studio index view
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult AjaxCreate(Studio studio)
+        {
+            db.Studios.Add(studio);
+            db.SaveChanges();
+            return Json(studio);
+        }
+
+        //*****************************************************EDIT********************************************************************//
+        //get a partial view for a studio edit with AJAX display
+        [HttpGet]
+        public PartialViewResult StudioEdit(int id)
+        {
+            Studio studio = db.Studios.Find(id);
+            return PartialView(studio);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult AjaxEdit(Studio studio)
+        {
+            db.Entry(studio).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json(studio);
         }
 
         protected override void Dispose(bool disposing)
